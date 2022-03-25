8ec49ba75dcbad4435ff4b3c2f33a6755ba110c4
Nello6 <85487743+Nello6@users.noreply.github.com>
2022-03-25 09:48:47 +0100

    Delete vapor_controller_API/sss-contoller-routing-API directory

--- vapor_controller_API/sss-contoller-routing-API/Sources/App/routes.swift
+++
@@ -1,8 +0,0 @@
-import Vapor
-
-func routes(_ app: Application) throws {
-    
-    try app.register(collection: PlanetController()) //Routing
-    try app.register(collection: APIController()) //Json API
-
-}
